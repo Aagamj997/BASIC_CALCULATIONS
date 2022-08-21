04e64a82264082f74a816b8744c990fd93ef57cf
Aagam Jain <80820614+Aagamj997@users.noreply.github.com>
2022-08-21 09:06:37 +0530

    Add files via upload

---
+++ math_functions_max.java
@@ -0,0 +1,14 @@
+public class math_functions_max{
+    public static void main (){
+        int a=5;
+        int b=6;
+        int c=Math.max(a,b);
+        System.out.print(c);
+        
+        
+        double d=5.5;
+        double e=6.6;
+        double f=Math.max(d,e);
+        System.out.print(f);
+    }
+}
