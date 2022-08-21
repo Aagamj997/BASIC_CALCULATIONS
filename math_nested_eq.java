dd249bc725d5eecff0b7fad7c506896e08f05e36
Aagam Jain <80820614+Aagamj997@users.noreply.github.com>
2022-08-21 09:05:52 +0530

    Add files via upload

---
+++ math_nested_eq.java
@@ -0,0 +1,9 @@
+public class math_nested_eq{
+    public static void main(){
+        double x=4;
+        double y=2;
+        double z=Math.pow(y,2);
+        double ans=Math.sqrt(x+3*y);
+        System.out.print(ans);
+    }
+}
